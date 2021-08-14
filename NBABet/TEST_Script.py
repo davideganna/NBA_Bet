66a25353a89ebd8fd0f3e7d6caa4d2ab93c2e62a
davideganna <52606991+davideganna@users.noreply.github.com>
2021-08-14 20:57:14 +0200

    added 2017 dataset

--- NBABet/TEST_Script.py
+++ NBABet/TEST_Script.py
@@ -1,6 +1,13 @@
 import Helper
+import pandas as pd
 
-folder = 'past_data/2020_2021/'
-Helper.split_stats_per_game(folder)
-Helper.add_odds_to_split_df()
+folder = 'past_data/2017_2018/'
+
+df_new = pd.read_csv(folder + 'split_stats_per_game_2017.csv', index_col=False)
+df_old = pd.read_csv('past_data/merged_seasons/2018_to_2021_Stats.csv', index_col=False)
+
+df_new.drop('Date', axis=1, inplace=True)
+
+df = pd.concat([df_new, df_old], axis=0)
+df.to_csv('past_data/merged_seasons/2017_to_2021_Stats.csv', index=False)
 
--- NBABet/get_past_datasets.py
+++ NBABet/get_past_datasets.py
@@ -113,9 +113,9 @@ df.to_csv(folder + 'stats_per_game_2017.csv', index=False)
 
 Helper.split_stats_per_game(folder)
 
-df_new = pd.read_csv(folder + 'split_stats_per_game_2017.csv')
-df_old = pd.read_csv('past_data/merged_seasons/2018_to_2021_Stats.csv')
+df_new = pd.read_csv(folder + 'split_stats_per_game_2017.csv', index_col=False)
+df_old = pd.read_csv('past_data/merged_seasons/2018_to_2020_Stats.csv', index_col=False)
 
 df = pd.concat([df_new, df_old], axis=0)
-df.to_csv('past_data/merged_seasons/2017_to_2021_Stats.csv', index=False)
+df.to_csv('past_data/merged_seasons/2017_to_2020_Stats.csv', index=False)
 
