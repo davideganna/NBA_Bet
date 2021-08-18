a80fd6fb29747537cb5889fbc444a90167edd97e
davideganna <52606991+davideganna@users.noreply.github.com>
2021-08-18 18:55:20 +0200

    Added features to all datasets

--- NBABet/TEST_Script.py
+++ NBABet/TEST_Script.py
@@ -7,9 +7,9 @@ df_2017 = pd.read_csv('past_data/2017_2018/split_stats_per_game_2017.csv')
 df_2018 = pd.read_csv('past_data/2018_2019/split_stats_per_game_2018.csv')
 df_2019 = pd.read_csv('past_data/2019_2020/split_stats_per_game_2019.csv')
 df_2020 = pd.read_csv('past_data/2020_2021/split_stats_per_game.csv')
-merged_19 = pd.read_csv('past_data/merged_seasons/2017_to_2019_Stats.csv', sep=';')
-merged_20 = pd.read_csv('past_data/merged_seasons/2017_to_2020_Stats.csv', index_col=False)
-merged_21 = pd.read_csv('past_data/merged_seasons/2017_to_2021_Stats.csv', index_col=False)
+merged_19 = pd.read_csv('past_data/merged_seasons/2017_to_2019_Stats.csv')
+merged_20 = pd.read_csv('past_data/merged_seasons/2017_to_2020_Stats.csv')
+merged_21 = pd.read_csv('past_data/merged_seasons/2017_to_2021_Stats.csv')
 
 df_list = [
     df_2017,
